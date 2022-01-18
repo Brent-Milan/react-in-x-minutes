77c7d6c738149955c9c27e1376d3366b5a44d7cf
Brent-Milan <Bmilan24@gmail.com>
2022-01-18 12:50:06 -0500

    localstorage set up

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
 import './App.css';
 import TodoList from './components/TodoList';
 import ToDosCounter from './components/ToDosCounter';
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 function App() {
   const [toDos, setTodos] = useState([ { id: 1, name: 'Dishes', complete: false}, { id: 2, name: 'Laundry', complete: false}]);
+  const inputRef = useRef();
+  const LOCAL_STORAGE_KEY = 'todoApp.todos';
+
+  const handleOnClick = (e) => {
+    const name = inputRef.current.value;
+    if (name === '') return
+    setTodos(prevTodos => {
+      return [...prevTodos, { id: Math.random, name: name, complete: false }];
+    })
+    inputRef.current.value = null;
+  }
+
+  useEffect( () => {
+    const storedToDos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (storedToDos)  setTodos(storedToDos);
+  }, []
+  )
+
+  useEffect(() => {
+   localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(toDos))
+ }, [toDos]);
+
   return (
     <>
       <TodoList toDos={toDos}></TodoList>
-      <input type="text"></input>
-      <button>Add Todo</button>
+      <input ref={inputRef} type="text"></input>
+      <button onClick={handleOnClick}>Add Todo</button>
       <button>Clear Completed Todos</button>
       <div>
         <ToDosCounter toDos={toDos}></ToDosCounter>
